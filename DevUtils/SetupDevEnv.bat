889b08eee5776385fb8a3887e275648e398b805f
palicone <primoz@palic.si>
2025-08-03 21:01:56 +0200

    Added src Python argument to the env setup script

--- DevUtils/SetupDevEnv.bat
+++ DevUtils/SetupDevEnv.bat
@@ -5,6 +5,22 @@ set THIS_FOLDER=%~dp0
 for %%I in ("%THIS_FOLDER%..\..\..\tools\Python\python.exe") do set "SRC_PYTHON=%%~fI"
 for %%I in ("%THIS_FOLDER%..\..\..\tools\bmcpptPyVenv") do set "VENV_PATH=%%~fI"
 
+IF [%1] EQU [] (
+  echo Source Python not provided
+  call :usage
+  echo Using: %SRC_PYTHON%
+  pause
+) else (
+  set SRC_PYTHON=%1
+)
+
+IF NOT EXIST "%SRC_PYTHON%" (
+  echo Source Python executable not found: %SRC_PYTHON%
+  call :usage
+  pause
+  exit /b 1
+)
+
 echo Using Python executable: %SRC_PYTHON%
 echo Creating Python virtual environment in: %VENV_PATH%
 %SRC_PYTHON% -m venv %VENV_PATH%
@@ -24,6 +40,15 @@ penvstp ^
 --tools-folder=%THIS_FOLDER%..\..\..\tools ^
 --externals-folder=%THIS_FOLDER%..\..\..\externals ^
 %THIS_FOLDER%env_setup_actions.json
+
+goto end
+
+:usage
+echo ^> Usage: %THIS_PATH% [SOURCE_PYTHON]
+echo ^> Example: %THIS_PATH% python.exe
+goto :eof
+
+:end
 
 cmd /k
 
